8c2b8966d184725b376563cfbe9983f1b9fc5510
Kim Palao <kimfarhantpalao@gmail.com>
2024-03-09 04:02:38 +0800

    Move to server-side filtering

--- app/baserow_client.py
+++ app/baserow_client.py
@@ -5,3 +5,20 @@ baserow = Baserow(url='https://baserow.kimpalao.com', token=settings.BASEROW_TOK
 
 users_table = baserow.get_table(513)
 hardware_table = baserow.get_table(514)
+
+def get_baserow_operator(op):
+    match op:
+        case 'equals':
+            return 'equal'
+        case 'contains':
+            return 'contains'
+        case 'dateIs':
+            return 'date_equal'
+        case 'dateIsNot':
+            return 'date_not_equal'
+        case 'dateBefore':
+            return 'date_before'
+        case 'dateAfter':
+            return 'date_after'
+        case _:
+            return op
